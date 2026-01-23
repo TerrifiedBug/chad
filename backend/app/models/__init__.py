b00bd1860be4679d16aef1fd2f8576ab44c04577
TerrifiedBug <admin@terrifiedbug.com>
2026-01-23 10:19:02 +0000

    fix: export RuleSource from models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,7 +3,8 @@ from app.models.audit_log import AuditLog
 from app.models.index_pattern import IndexPattern
 from app.models.login_attempt import LoginAttempt
 from app.models.role_permission import RolePermission, DEFAULT_ROLE_PERMISSIONS
-from app.models.rule import Rule, RuleStatus, RuleVersion
+from app.models.rule import Rule, RuleSource, RuleStatus, RuleVersion
+from app.models.rule_comment import RuleComment
 from app.models.rule_exception import ExceptionOperator, RuleException
 from app.models.setting import Setting
 from app.models.user import User, UserRole
@@ -17,7 +18,9 @@ __all__ = [
     "LoginAttempt",
     "RolePermission",
     "Rule",
+    "RuleComment",
     "RuleException",
+    "RuleSource",
     "RuleStatus",
     "RuleVersion",
     "Setting",
